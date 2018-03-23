68a9fde8cf820080552e99b0d2c69e6775654270
Frank Valdez <fvaldez421@gmail.com>
2018-03-23 14:01:02 -0700

    added some routes, created files and index.js for pages

--- client/src/components/WelcomeDetail/WelcomeDetail.js
+++ client/src/components/WelcomeDetail/WelcomeDetail.js
@@ -1,1 +1,10 @@
-WelcomeDetail.js
+import React from "react";
+import "./WelcomeDetail.css";
+
+const WelcomeDetail = () => (
+	<div>
+		<p>This is the welcome message</p>
+	</div>
+	);
+
+export default WelcomeDetail;
---
+++ client/src/components/pages/About/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css';
+import Wrapper from "../../Wrapper";
+
+const About = (props) => (
+	<Wrapper>
+	<h1>About</h1>
+	<div>About go here</div>
+	<div>About go here</div>
+	<div>About go here</div>
+	<div>About go here</div>
+	<div>About go here</div>
+	<div>About go here</div>
+	<div>About go here</div>
+
+	</Wrapper>
+) 
+export default About;
