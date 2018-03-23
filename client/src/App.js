7fb6bc7f2c50555ea888b13c96d4041153522aa1
Frank Valdez <fvaldez421@gmail.com>
2018-03-23 14:49:21 -0700

    added more routing and links

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Welcome from "./components/pages/Welcome";
 import About from "./components/pages/About";
+import Region from "./components/pages/Region";
 import Bills from "./components/pages/Bills";
 import "./App.css";
 
@@ -14,12 +15,14 @@ const App = () => (
       <div className="testHeader">( Test Header used for Routing )  
       <Header />
         <a href="/">|||||||  Home  </a>
+        <a href="/region">||  Region </a>        
         <a href="/bills">||  Bills </a>
         <a href="/about">||  About</a>
       </div>
       <Route exact path="/" component={Welcome} />
       <Route exact path="/about" component={About} />
-      <Route exact path="/bills" component={Bills} />
+      <Route exact path="/region" component={Region} />
+      <Route path="/bills:id" component={Bills} />
       <Footer />
     </div>
   </Router>
--- client/src/components/pages/Bills/Bills.js
+++ client/src/components/pages/Bills/Bills.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './Bills.css';
 import Wrapper from "../../Wrapper";
+import BillDetail from "../BillDetail";
 
 const Bills = (props) => (
 	<Wrapper>
 	<h1>Bills</h1>
-	<div>Bills go here</div>
-	<div>Bills go here</div>
-	<div>Bills go here</div>
-	<div>Bills go here</div>
-	<div>Bills go here</div>
-	<div>Bills go here</div>
-	<div>Bills go here</div>
-
+	<BillDetail />
 	</Wrapper>
 ) 
 export default Bills;
