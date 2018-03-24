92ffb8083ae2063571d84cee25b4acba9ab76bfa
David Morrill <judimorrill@Judis-MacBook-Pro.local>
2018-03-24 12:47:42 -0700

    added index.js to BillBlock

--- client/src/components/pages/Bills/Bills.js
+++ client/src/components/pages/Bills/Bills.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Bills.css';
 import Wrapper from "../../Wrapper";
-import BillDetail from "../BillDetail";
+import BillBlock from "../BillBlock";
 
 const Bills = (props) => (
 	<Wrapper>
 	<h1>Bills</h1>
-	<BillDetail />
+	<BillBlock />
 	</Wrapper>
 ) 
 export default Bills;
