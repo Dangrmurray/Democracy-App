250fa027887209380f01e40480bc6ab7d392d31e
Daniel Murray <danielgrmurray@gmail.com>
2018-03-26 11:59:32 -0700

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 const express = require("express");
 const path = require("path");
+
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+
+
+
 const PORT = process.env.PORT || 3001;
+
+
 const app = express();
+
+app.use(cookieSession({
+	maxAge: 24*60*60*1000,
+	keys: [keys.session.cookieKey]
+}));
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () =>{
+	console.log('connected to mongodb');
+});
+
+//set up routes
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
