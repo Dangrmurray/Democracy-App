1624778ef60775d7f266d541cd60fe4862a0ab7f
Jason Kunkel <jasonakunkel@outlook.com>
2018-03-29 11:23:54 -0700

    updated region.js page to match master

--- client/src/components/pages/Region/Region.js
+++ client/src/components/pages/Region/Region.js
@@ -3,37 +3,18 @@ import './Region.css';
 import Wrapper from "../../Wrapper";
 
 const Region = (props) => (
-	<Wrapper>
-
-	<div className="container">
-		<div className="row-justify-content-md-center border">
-			<div className="col-md-1">
-			<h2 className="panel-title"><strong><center>Choose Your Region</center></strong></h2>
-		<div className="container">
-			<div className="row-justify-content-md-center border">
-				<div className="col-md-1">
-				<h2 className="panel-title"><strong><center>Choose Your Region</center></strong></h2>
-				</div>
-
-			</div>
-		</div>
-		<div className="container">
-    	    <div className="row">
-    	        <div className="col-4">
-    	            <div className="card bill">City</div>
-    	        </div>
-    	        <div className="col-4">
-    	            <div className="card bill">State</div>
-    	        </div>
-    	        <div className="col-4">
-    	            <div className="card bill">Federal</div>
-    	        </div>
-    	    </div>
-		</div>
-	
-);
-
-
+    <Wrapper>
+        <div className="container">
+            <div className="row-justify-content-md-center border">
+                <div className="col-md-1">
+                <h2 className="panel-title"><strong><center>Choose Your Region</center></strong></h2>
+                </div>
+            </div>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <div className="card bill">City</div>
                 </div>
                 <div className="col-4">
                     <div className="card bill">State</div>
@@ -43,6 +24,7 @@ const Region = (props) => (
                 </div>
             </div>
         </div>
-     </Wrapper>
-) 
+    </Wrapper>
+);
+
 export default Region;
