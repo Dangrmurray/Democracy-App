8bd64018e585399bb518ed47a7a864377a9b3994
Frank Valdez <fvaldez421@gmail.com>
2018-03-30 14:55:52 -0700

    stuffs

--- models/bill.js
+++ models/bill.js
@@ -29,10 +29,6 @@ var BillSchema = new Schema({
 		type: String,
 		unique: false
 	},
-	sponsor_url: {
-		type: String,
-		unique: false
-	},
 	congressdotgov_url: {
 		type: String,
 		unique: false
@@ -43,12 +39,12 @@ var BillSchema = new Schema({
 	},
 	summary_short: {
 		type: String,
-		max: 400,
+		max: 2000,
 		unique: false
 	},
 	summary: {
 		type: String,
-		max: 800,
+		max: 10000,
 		unique: false
 	},
 	active: {
