80b12078fb4924e126b685ccb51396d790f37f7f
Daniel Murray <danielgrmurray@gmail.com>
2018-04-02 10:30:14 -0700

    fix api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -34,7 +34,6 @@ export default {
     return (axios.get("https://api.propublica.org/congress/v1/115/house/bills/active.json", {headers: {"X-API-Key": "7BoKxES5grHLDPrdYNsMrvhgNQuN5aZL0Jdr0ZDU"}})
     .then(function(bills) {
       console.log(bills)
-      axios.post("/api/bills", bills)
       return bills
     }))
   },
