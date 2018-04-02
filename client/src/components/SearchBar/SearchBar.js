e3ebbe0a15c9ec0cad5ba28824674501be4f86ec
David Morrill <judimorrill@Judis-MacBook-Pro.local>
2018-04-02 10:49:53 -0700

    good stuff

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import API from "../../utils/API.js"
 import Bills from "../pages/Bills/"
+
+// I need to set the bills component state -> bills array from the searchBar.js file
+// .The searchBar make an api call and returns a bills array that I would like 
+// to set the bills array on the Bills.js component
+
 // import bills
 
 //access the bills component and change it .bills to the result of this search bills function. Change bills variable
@@ -28,36 +33,39 @@ class SearchBar extends Component {
                 );
 
                 // Log all bills in DB
-                for (let i = 0; i < bills.length; i++) {
-                    let currentBill = bills[i];
-                    // debugger;
-                    API.logBills({
-                        name: currentBill.title,
-                        key: currentBill.bill_id,
-                        sponsor_name: currentBill.sponsor_name,
-                        sponsor_state: currentBill.sponsor_state,
-                        sponsor_party: currentBill.sponsor_party,
-                        sponsor_title: currentBill.sponsor_title,
-                        sponsor_url: currentBill.sponsor_url,
-                        congressdotgov_url: currentBill.congressdotgov_url,
-                        govtrack_url: currentBill.govtrack_url,
-                        summary_short: currentBill.summary_short,
-                        summary: currentBill.summary,
-                        active: currentBill.active,
-                        introduced_date: currentBill.introduced_date,
-                        latest_major_action: currentBill.latest_major_action,
-                        latest_major_action_date: currentBill.latest_major_action_date
-                    })
-                        .then(res => {
-                            console.log(res);
+                // for (let i = 0; i < bills.length; i++) {
+                //     let currentBill = bills[i];
+                //     // debugger;
+                //     API.logBills({
+                //         name: currentBill.title,
+                //         key: currentBill.bill_id,
+                //         sponsor_name: currentBill.sponsor_name,
+                //         sponsor_state: currentBill.sponsor_state,
+                //         sponsor_party: currentBill.sponsor_party,
+                //         sponsor_title: currentBill.sponsor_title,
+                //         sponsor_url: currentBill.sponsor_url,
+                //         congressdotgov_url: currentBill.congressdotgov_url,
+                //         govtrack_url: currentBill.govtrack_url,
+                //         summary_short: currentBill.summary_short,
+                //         summary: currentBill.summary,
+                //         active: currentBill.active,
+                //         introduced_date: currentBill.introduced_date,
+                //         latest_major_action: currentBill.latest_major_action,
+                //         latest_major_action_date: currentBill.latest_major_action_date
+                //     })
+                //         .then(res => {
+                //             console.log(res);
 
-                        })
-                        .catch(err => console.log(err))
-                }
+                //         })
+                //         .catch(err => console.log(err))
+                // }
 
             })
             .catch(err => console.log(err));
         // set bills = result
+        // var parent = this._reactInternalInstance._currentElement._owner._instance;
+        // console.log("parent:", parent);
+        console.log(Bills.state)
     }  
     
 
