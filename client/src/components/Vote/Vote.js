a81f9705a7c7c13cc94964774c72caa4ffe67631
Fintech79 <abdelrahman.faisal@gmail.com>
2018-04-02 10:59:00 -0700

    votes

--- client/src/components/Vote/Vote.js
+++ client/src/components/Vote/Vote.js
@@ -6,121 +6,80 @@ import Chart from '../VoteNumChart';
 
 
 class Vote extends React.Component {
-<<<<<<< HEAD
-  constructor(){
-    super();
-    this.state = {
+ constructor(props){
+   super(props);
+   this.state = {  
+     chartDataNum:{}
+   }
+ }
+ 
 
-      // add to State what would be displayed tally of the onClick 
-     // think about when somebody clicks the yes delta State btn
-      
-=======
-  constructor(props){
-    super(props);
-    this.state = {   
->>>>>>> d9fe48a7c45aac8769cce5704fbaf5b6f030b5c4
-      chartDataNum:{}
-    }
-  }
-  
+ componentWillMount(){
+   this.getChartDataNum();
+ }
 
-  componentWillMount(){
-    this.getChartDataNum();
-  }
+ getChartDataNum(){
+   // Ajax calls here
+   this.setState({
+     chartDataNum:{
+       labels: ['Yes', 'No', 'Undecided',],
+       datasets:[
+         {
+           label:'Votes',
+           data:[
+             67,
+             567,
+             987
+           ],
+           backgroundColor:[
+             'rgba(119, 191, 198, 1)',
+             'rgba(238, 38, 37, 1)',
+             'rgba(255, 240, 200, 1)'
+           ]
+         },
+       ]
+     }
+   });
+ }
 
-  getChartDataNum(){
-    // Ajax calls here
-    this.setState({
-      chartDataNum:{
-        labels: ['Yes', 'No', 'Undecided',],
-        datasets:[
-          {
-            label:'Votes',
-            data:[
-              417594,
-              105162,
-              95072
-            ],
-            backgroundColor:[
-              'rgba(119, 191, 198, 1)',
-              'rgba(238, 38, 37, 1)',
-              'rgba(255, 240, 200, 1)'
-            ]
-          },
-        ]
-      }
-    });
-  }
-<<<<<<< HEAD
-    yesVote = (event) => {
-      event.preventDefault();
-    }
+ voteYes(){
+   console.log('You voted Yes');
+ }
+ 
+ voteNo(){
+   console.log('You voted No');
+ }
 
-    noVote = (event) => {
-      event.preventDefault();
-    }
-
-    undecidedVote = (event) => {
-      event.preventDefault();
-    }
-=======
-
-  voteYes(){
-    console.log('You voted Yes');
-  }
->>>>>>> d9fe48a7c45aac8769cce5704fbaf5b6f030b5c4
-  
-  voteNo(){
-    console.log('You voted No');
-  }
-
-  voteUndecided(){
-    console.log('You voted Perhaps?');
-  }
-   
-    render() {
-      console.log(this.props.userId)
-      return (
-       
-        <div className="col-6 mx-auto voteBlock">
-<<<<<<< HEAD
-          <div className=" loggedOut">
-            <h5>Login To Vote</h5>
-          </div>
-
-          <div className="loggedIn">
-            <button onClick={this.yesVote} className="btn btn-success">YES</button>
-            <button onClick={this.noVote} className="btn btn-danger">NO</button>
-            <button onClick={this.undecidedVote} className="btn btn-secondary">UNDECIDED</button>
-          </div>
-          <div className="voteOverview">
-            <h5>Vote Summary Graph</h5>
-              <Chart chartData={this.state.chartDataNum} location="Votes" legendPosition="top"/>
-            
-          </div>
-=======
-          {
-          (!this.props.userId) 
-          ? <div className=" loggedOut">
-              <h5>Login To Vote</h5>
-            </div>
-          : <div>
-              <div className="loggedIn">
-                <button onClick={this.voteYes} className="btn btn-success">YES</button>
-                <button onClick={this.voteNo} className="btn btn-danger">NO</button>
-                <button onClick={this.voteUndecided} className="btn btn-secondary">UNDECIDED</button>
-              </div>
-              
-              <div className="voteOverview">
-                <h5>Vote Summary Graph</h5>
-                  <Chart chartData={this.state.chartDataNum} location="Votes" legendPosition="top"/>
-              </div>
-            </div>
-          }
->>>>>>> d9fe48a7c45aac8769cce5704fbaf5b6f030b5c4
-        </div>
-      );
-    }
-  }
-  
-  export default Vote;
+ voteUndecided(){
+   console.log('You voted Perhaps?');
+ }
+ 
+   render() {
+     console.log(this.props.userId)
+     return (
+     
+       <div className="col-6 mx-auto voteBlock">
+         {
+         (!this.props.userId)
+         ? <div className=" loggedOut">
+             <h5>Login To Vote</h5>
+           </div>
+         : <div>
+             <div className="loggedIn">
+               <button onClick={this.voteYes} className="btn btn-success">YES</button>
+               <button onClick={this.voteNo} className="btn btn-danger">NO</button>
+               <button onClick={this.voteUndecided} className="btn btn-secondary">UNDECIDED</button>
+             </div>
+             
+             <div className="voteOverview">
+               <h5>Vote Summary Graph</h5>
+                 <Chart chartData={this.state.chartDataNum} location="Votes" legendPosition="top"/>
+             </div>
+           </div>
+         }
+       </div>
+     );
+   }
+ }
+ 
+ export default Vote;
